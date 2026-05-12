1fdb851cc4bb07c5d5adbb048e54f5f33cac807c
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 15:48:58 +0530

    feat: contants/index.js completed

---
+++ src/constants/index.js
@@ -0,0 +1,36 @@
+export const ROLES = {
+    SUPER_ADMIN: "SUPER_ADMIN",
+    ADMIN: "ADMIN",
+    TEACHER: "TEACHER",
+    USER: "USER",
+}
+
+export const PLANS = {
+    BASIC: "BASIC", 
+    GROWTH: "GROWTH",
+    PRO: "PRO",
+}
+
+export const ATTENDANCE_STATUS = {
+    PRESENT: "PRESENT",
+    ABSENT: "ABSENT",
+    MEDICAL:  "MEDICAL",
+    EXCUSED: "EXCUSED",
+    RECOVERED: "RECOVERED",
+}
+
+export const ATTENDANCE_TYPES = {
+    MORNING: "MORNING",
+    AFTERNOON: "AFTERNOON",
+    PERIOD: "PERIOD",
+}
+
+export const DAYS_OF_WEEK = [
+    { label: "Sunday",    value: 0 },
+    { label: "Monday",    value: 1 },
+    { label: "Tuesday",   value: 2 },
+    { label: "Wednesday", value: 3 },
+    { label: "Thursday",  value: 4 },
+    { label: "Friday",    value: 5 },
+    { label: "Saturday",  value: 6 },
+]
