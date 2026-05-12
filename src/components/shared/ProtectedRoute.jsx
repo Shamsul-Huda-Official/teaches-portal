814317608bdec3c6b47b847e476822af03b0153d
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 21:21:59 +0530

    feat: Protected Routes completed

--- src/components/shared/ProtectedRoute.jsx
+++ src/components/shared/ProtectedRoute.jsx
@@ -1,7 +1,15 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../../store/AuthContext"
+
 export default function ProtectedRoute() {
-    return (
-        <div>
-            <h1>Protected Route</h1>
-        </div>
-    )
+    const { isAuthenticated, loading } = useAuth();
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-slate-950">
+                <div className="w-8 h-8 border-2 border-blue-500 rounded-full border-t-transparent animate-spin" />
+            </div>
+        )
+    }
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />
 }
