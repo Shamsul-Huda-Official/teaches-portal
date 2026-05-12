97245ef300e7d5d694e959a6aac0324a119bd81a
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 21:29:00 +0530

    feat: PublicRoutes.jsx completed

--- src/components/shared/PublicRoute.jsx
+++ src/components/shared/PublicRoute.jsx
@@ -1,7 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../../store/AuthContext"
+
 export default function PublicRoute() {
-    return (
-        <div>
-            <h1>Public Route</h1>
-        </div>
-    )
+    const { isAuthenticated, loading } = useAuth();
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-slate-950">
+                <div className="w-8 h-8 border-2 border-blue-500 rounded-full border-t-transparent animate-spin" />
+            </div>
+        )
+    }
+    return isAuthenticated ? <Navigate to="/dashboard" replace/>: <Outlet /> 
 }
